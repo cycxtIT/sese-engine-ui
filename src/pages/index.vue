a940016569074d60b51c2d9fdbc16829afc3e785
cycxtIT <90236905+cycxtIT@users.noreply.github.com>
2022-06-17 19:13:53 +0800

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
 import { useEnter } from '~/composables/search'
 import { useSearchStore } from '~/stores/search'
-
 import { bannerUrl } from '~/config'
-
 const { enter } = useEnter()
 const search = useSearchStore()
 const keyword = ref(search.savedKeyword)
-
 const { t } = useI18n()
 </script>
 
 <template>
   <div class="flex flex-col justify-center items-center" h="full">
-    <img width="280" height="156" class="w-70 filter drop-shadow" m="-b-4" :src="https://s1.328888.xyz/2022/06/17/0AgBi.jpg" alt="ico">
+    <img width="280" height="156" class="w-70 filter drop-shadow" m="-b-4" :src="bannerUrl" alt="Rimo And XiaoYun">
 
     <InputBox v-model="keyword" m="t-0 b-4" :enter="() => { enter(keyword) }" />
 
